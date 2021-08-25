614a801199f6ac5ecd787e7ca61c32a4e46205d2
Conner Chappell <conner.chappell123@gmail.com>
2021-08-25 15:45:33 -0600

    Initial practice session

---
+++ src/codeJournal.js
@@ -0,0 +1,12 @@
+// Arrow Functions
+function callMe(name) {
+    console.log(name)
+}
+
+const callMe2 = function(name) {
+    console.log(name)
+}
+
+const callMe3 = (name) => {
+    console.log(name)
+}
