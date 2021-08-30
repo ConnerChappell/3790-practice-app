c64b6d712ede2e91ab6f6bf05d88cc53dad5934e
Conner Chappell <conner.chappell123@gmail.com>
2021-08-30 15:41:00 -0600

    Destructuring practice in code journal

--- src/codeJournal.js
+++ src/codeJournal.js
@@ -10,3 +10,13 @@ const callMe2 = function(name) {
 const callMe3 = (name) => {
     console.log(name)
 }
+
+// Destructuring
+function getScores() {
+    return [70, 80, 90, 100]
+}
+let [x, y, ...args] = getScores()
+console.log(x) // 70
+console.log(y) // 80
+console.log(args) // [90, 100]
+
