a6592235a917e903b0ac6781882633669820801d
Conner Chappell <conner.chappell123@gmail.com>
2021-09-21 14:09:28 -0600

    Added modal and events

--- src/components/SenatorCard.js
+++ src/components/SenatorCard.js
@@ -4,45 +4,39 @@ import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import CardActions from '@mui/material/CardActions'
-import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import InfoIcon from '@mui/icons-material/Info'
 
 import '../App.css'
 
-const bull = (
-    <Box
-        component="span"
-        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}>
-        •
-    </Box>
-)
-
-const SenatorCard = (senator) => {
+const SenatorCard = (props) => {
     // const { firstName, lastName, id } = props
+
+    const handleInfoClick = () => {
+        console.log('Clicked')
+        props.modalFunction()
+    }
 
     return (
         <Card
-            key={senator.id}
             className="card"
             sx={{
                 width: 300,
-                bgcolor: 'primary.dark',
-                '&:hover': {
-                    backgroundColor: 'primary.main',
-                    opacity: [0.9, 0.8, 0.7],
-                },
+                m: 1.5,
+                // '&:hover': {
+                //     opacity: [0.9, 0.8, 0.7],
+                // },
             }}>
             <CardContent>
                 <Typography sx={{ fontSize: 24 }} color="text.primary">
-                    {senator.first_name} {senator.last_name}
-                </Typography>
-
-                <Typography variant="h6" component="div">
-                    be{bull}nev{bull}o{bull}lent
+                    {props.senator.first_name} {props.senator.last_name}
                 </Typography>
             </CardContent>
 
             <CardActions>
-                <Button size="small">Learn More</Button>
+                <IconButton onClick={handleInfoClick} >
+                    <InfoIcon />
+                </IconButton>
             </CardActions>
         </Card>
     )
--- src/components/SenatorContainer.js
+++ src/components/SenatorContainer.js
@@ -1,9 +1,32 @@
-import React from 'react'
+// import React, { useState } from 'react'
+import * as React from 'react'
 import { senators } from '../data/senate'
 import SenatorCard from './SenatorCard'
 import Box from '@mui/material/Box'
+import Modal from '@mui/material/Modal'
+import Typography from '@mui/material/Typography'
+
+const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    tranfrom: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    borderRadius: '5px',
+    boxShadow: 24,
+    p: 4,
+}
 
 const SenatorContainer = () => {
+    const [openModal, setOpenModal] = React.useState(false)
+    const handleOpenModal = () => {
+        setOpenModal(true)
+    }
+    const handleCloseModal = () => {
+        setOpenModal(false)
+    }
+
     return (
         <Box sx={{
             display: 'flex',
@@ -11,9 +34,16 @@ const SenatorContainer = () => {
         }}>
             {senators.map((senator) => {
                 return (
-                    <SenatorCard {...senator} />
+                    <SenatorCard modalFunction={handleOpenModal} senator={{...senator}} />
                 )
             })}
+            <Modal open={openModal} onClose={handleCloseModal} >
+                <Box sx={modalStyle} >
+                    <Typography varient="h6" >
+                        Senator Information
+                    </Typography>
+                </Box>
+            </Modal>
         </Box>
     )
 }
